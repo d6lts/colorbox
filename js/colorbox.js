aae70697b27dc77c3fa93579ee793c76303cc438
Fredrik Jonsson <frjo@xdeb.org>
2010-09-21 13:05:45 +0000

    Minor cleanups, extra line feed etc.

--- js/colorbox.js
+++ js/colorbox.js
@@ -2,7 +2,6 @@
 (function ($) {
 
 Drupal.behaviors.initColorbox = function (context) {
-  var settings = Drupal.settings.colorbox;
   $('a, area, input', context)
     .filter('.colorbox:not(.initColorbox-processed)')
     .addClass('initColorbox-processed')
