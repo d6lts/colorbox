5686c35419dcf3e114fe7472e49b2c55e7bf2645
Fredrik Jonsson <frjo@xdeb.org>
2010-12-02 09:15:14 +0000

    Added context to jQuery selectors in the styles js, speed things up a bit I belive. Minor fixes to coding style as well.

--- styles/default/colorbox_default_style.js
+++ styles/default/colorbox_default_style.js
@@ -2,19 +2,19 @@
 (function ($) {
 
 Drupal.behaviors.initColorboxDefaultStyle = function (context) {
-  $(document).bind('cbox_complete', function(){
+  $(document).bind('cbox_complete', function () {
     // Only run if there is a title.
-    if ($('#cboxTitle:empty').length == false) {
-      setTimeout(function(){ $('#cboxTitle').slideUp() }, 1500);
-      $('#cboxLoadedContent').bind('mouseover', function(){
-        $('#cboxTitle').slideDown();
+    if ($('#cboxTitle:empty', context).length == false) {
+      setTimeout(function () { $('#cboxTitle', context).slideUp() }, 1500);
+      $('#cboxLoadedContent', context).bind('mouseover', function () {
+        $('#cboxTitle', context).slideDown();
       });
-      $('#cboxOverlay').bind('mouseover', function(){
-        $('#cboxTitle').slideUp();
+      $('#cboxOverlay', context).bind('mouseover', function () {
+        $('#cboxTitle', context).slideUp();
       });
     }
     else {
-      $('#cboxTitle').hide();
+      $('#cboxTitle', context).hide();
     }
   });
 };
--- styles/stockholmsyndrome/colorbox_stockholmsyndrome.js
+++ styles/stockholmsyndrome/colorbox_stockholmsyndrome.js
@@ -1,18 +1,18 @@
 // $Id$
 (function ($) {
 
-Drupal.behaviors.initColorboxDefaultStyle = function (context) {
-  $(document).bind('cbox_open', function() {
+Drupal.behaviors.initColorboxStockholmsyndromeStyle = function (context) {
+  $(document).bind('cbox_open', function () {
     // Hide close button initially.
-    $('#cboxClose').css('opacity', 0);
+    $('#cboxClose', context).css('opacity', 0);
   });
-  $(document).bind('cbox_load', function() {
+  $(document).bind('cbox_load', function () {
     // Hide close button. (It doesn't handle the load animation well.)
-    $('#cboxClose').css('opacity', 0);
+    $('#cboxClose', context).css('opacity', 0);
   });
-  $(document).bind('cbox_complete', function() {
+  $(document).bind('cbox_complete', function () {
     // Show close button with a delay.
-    $('#cboxClose').fadeTo('fast', 0, function() {$(this).css('opacity', 1)});
+    $('#cboxClose', context).fadeTo('fast', 0, function () {$(this).css('opacity', 1)});
   });
 };
 
