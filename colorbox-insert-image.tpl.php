7b8e3baa2edc2863ea1c1dc3f7a8368fe23e89e7
Fredrik Jonsson <frjo@xdeb.org>
2012-01-15 19:47:06 +0300

    Issue #1081546, patch by rfay: Fix the call to theme_colorbox_insert_image() and add in the CSS classes from insert module.

--- colorbox-insert-image.tpl.php
+++ colorbox-insert-image.tpl.php
@@ -10,6 +10,7 @@
  * - $link_path: The URL to the image that Colorbox should open.
  * - $presetname: The ImageCache preset being used.
  * - $gallery_id: The ID of the Colorbox gallery.
+ * - $insert_class: The classes provided by the insert module.
  *
  * Note that ALT and Title fields should not be filled in here, instead they
  * should use placeholders that will be updated through JavaScript when the
@@ -21,4 +22,4 @@
  * - __description__: A description of the image, sometimes used as a caption.
  */
 ?>
-<a href="<?php print $link_path; ?>" title="__title__" class="colorbox colorbox-insert-image" rel="gallery-<?php print $gallery_id; ?>"><img src="<?php print $image_path; ?>" alt="__alt__" title="__title__" class="imagecache imagecache-<?php print $presetname; ?>" /></a>
+<a href="<?php print $link_path; ?>" title="__title__" class="colorbox colorbox-insert-image" rel="gallery-<?php print $gallery_id; ?>"><img src="<?php print $image_path; ?>" alt="__alt__" title="__title__" class="imagecache imagecache-<?php print "$presetname $insert_class"; ?>" /></a>
